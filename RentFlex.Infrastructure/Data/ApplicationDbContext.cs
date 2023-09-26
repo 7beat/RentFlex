577e3e4923f9fb9c016af86a7f3b2552db55e916
Tomasz Krzemiński <7beat123@wp.pl>
2023-09-26 22:50:49 +0200

    Added Converter

--- RentFlex.Infrastructure/Data/ApplicationDbContext.cs
+++ RentFlex.Infrastructure/Data/ApplicationDbContext.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using RentFlex.Infrastructure.Identity;
+using RentFlex.Utility.Converters;
 
 namespace RentFlex.Infrastructure.Data;
 internal class ApplicationDbContext : IdentityDbContext<ApplicationUser>
@@ -8,5 +9,12 @@ internal class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
     : base(options)
     {
+    }
+
+    protected override void ConfigureConventions(ModelConfigurationBuilder configurationBuilder)
+    {
+        configurationBuilder.Properties<DateOnly>()
+            .HaveConversion<DbDateOnlyConverter>()
+            .HaveColumnType("date");
     }
 }
