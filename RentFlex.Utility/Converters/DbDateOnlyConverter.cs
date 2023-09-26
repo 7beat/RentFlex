2dff75b8d2279b8d09edf6d9b849dcfffefb3ee0
Tomasz Krzemiński <7beat123@wp.pl>
2023-09-26 22:31:57 +0200

    Added Converter

---
+++ RentFlex.Utility/Converters/DbDateOnlyConverter.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
+
+namespace RentFlex.Utility.Converters;
+
+/// <summary>
+/// Converter that seamlessly transforms <see cref="DateOnly" /> instances to <see cref="DateTime"/> and vice versa.
+/// </summary>
+public class DbDateOnlyConverter : ValueConverter<DateOnly, DateTime>
+{
+    public DbDateOnlyConverter() : base(
+        d => d.ToDateTime(TimeOnly.MinValue),
+        d => DateOnly.FromDateTime(d))
+    { }
+}
