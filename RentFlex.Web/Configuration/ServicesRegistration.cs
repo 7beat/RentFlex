92ffac4f3058eb3c3e33977116c349d08ece26d6
Tomasz Krzemiński <7beat123@wp.pl>
2023-09-26 22:44:16 +0200

    tweak

---
+++ RentFlex.Web/Configuration/ServicesRegistration.cs
@@ -0,0 +1,11 @@
+﻿using RentFlex.Infrastructure;
+
+namespace RentFlex.Web.Configuration;
+
+public static class ServicesRegistration
+{
+    public static void RegisterServices(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.RegisterInfrastructure(configuration);
+    }
+}
