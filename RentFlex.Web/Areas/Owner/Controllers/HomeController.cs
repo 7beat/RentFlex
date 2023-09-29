d8b8bcb34d132069e27f7a58709e97d1b8a1c14b
Tomasz Krzemiński <7beat123@wp.pl>
2023-09-29 19:17:59 +0200

    Renamed Area

--- RentFlex.Web/Areas/Owner/Controllers/HomeController.cs
+++ RentFlex.Web/Areas/Owner/Controllers/HomeController.cs
@@ -4,7 +4,7 @@ using System.Diagnostics;
 
 namespace RentFlex.Web.Areas.User.Controllers;
 
-[Area("User")]
+[Area("Owner")]
 public class HomeController : Controller
 {
     private readonly ILogger<HomeController> _logger;
