156ae3a776a96f8faf986c2992cd0a041001c0f5
Tomasz Krzemiński <7beat123@wp.pl>
2023-09-29 20:34:26 +0200

    Added Configuration

--- RentFlex.Infrastructure/Data/ApplicationDbContext.cs
+++ RentFlex.Infrastructure/Data/ApplicationDbContext.cs
@@ -2,6 +2,7 @@
 using Microsoft.EntityFrameworkCore;
 using RentFlex.Infrastructure.Identity;
 using RentFlex.Utility.Converters;
+using System.Reflection;
 
 namespace RentFlex.Infrastructure.Data;
 internal class ApplicationDbContext : IdentityDbContext<ApplicationUser>
@@ -9,6 +10,12 @@ internal class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
     : base(options)
     {
+    }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+        builder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
     }
 
     protected override void ConfigureConventions(ModelConfigurationBuilder configurationBuilder)
---
+++ RentFlex.Infrastructure/Data/EntitiesConfiguration.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Newtonsoft.Json;
+using RentFlex.Domain.entities;
+
+namespace RentFlex.Infrastructure.Data;
+internal class EstatesConfiguration : IEntityTypeConfiguration<Estate>
+{
+    public void Configure(EntityTypeBuilder<Estate> builder)
+    {
+        builder.HasKey(e => e.Id);
+        builder.OwnsOne(e => e.Address);
+        builder.Property(e => e.ImageUrls)
+            .HasConversion(
+            x => JsonConvert.SerializeObject(x),
+            x => JsonConvert.DeserializeObject<ICollection<string>>(x)!);
+
+    }
+}
