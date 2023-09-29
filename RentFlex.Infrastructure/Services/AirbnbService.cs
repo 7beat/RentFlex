adc6041c02fa9a08ad1648eec9a2a865754c636e
Tomasz Krzemiński <7beat123@wp.pl>
2023-09-29 19:15:30 +0200

    Added concept Service

---
+++ RentFlex.Infrastructure/Services/AirbnbService.cs
@@ -0,0 +1,25 @@
+﻿using Polly;
+using Polly.CircuitBreaker;
+using Polly.Retry;
+
+namespace RentFlex.Infrastructure.Services;
+public class AirbnbService
+{
+    private static AsyncCircuitBreakerPolicy circutBreakerPolicy;
+    private static AsyncRetryPolicy retryPolicy;
+
+    private readonly HttpClient httpClient;
+
+    public AirbnbService()
+    {
+        retryPolicy = Policy
+            .Handle<Exception>()
+            .WaitAndRetryAsync(2, sd => TimeSpan.FromSeconds(20));
+
+    }
+
+    //public AirbnbService()
+    //{
+    //    httpClient = new HttpClient();
+    //}
+}
