d9265cf307ddad64d5fa785317bc4125b1db9d21
Tomasz Krzemiński <7beat123@wp.pl>
2023-09-30 19:05:14 +0200

    Added concpet Owned entity seeding

--- RentFlex.Infrastructure/Data/EntitiesConfiguration.cs
+++ RentFlex.Infrastructure/Data/EntitiesConfiguration.cs
@@ -9,7 +9,8 @@ internal class EstatesConfiguration : IEntityTypeConfiguration<Estate>
     public void Configure(EntityTypeBuilder<Estate> builder)
     {
         builder.HasKey(e => e.Id);
-        builder.OwnsOne(e => e.Address);
+        //builder.OwnsOne(e => e.Address); // check this
+        builder.Property(e => e.EstateType).HasConversion<string>();
         builder.HasMany(e => e.Rentals).WithOne(e => e.Estate).IsRequired(false);
         builder.Property(e => e.ImageUrls)
             .HasConversion(
@@ -17,5 +18,24 @@ internal class EstatesConfiguration : IEntityTypeConfiguration<Estate>
             x => JsonConvert.DeserializeObject<ICollection<string>>(x)!)
             .IsRequired(false);
 
+        builder.HasData(new Estate()
+        {
+            Id = Guid.Parse("555daf1f-c760-48d4-9fcf-410cec349f23"),
+            PropertyName = "TestProperty",
+            IsAvailable = true,
+            CostPerDay = 200,
+            EstateType = EstateType.Apartment,
+            OwnerId = Guid.Parse("6aa43469-b1c8-42b1-aa67-b7240a575f0a"),
+        });
+
+        builder.OwnsOne(e => e.Address).HasData(new
+        {
+            EstateId = Guid.Parse("555daf1f-c760-48d4-9fcf-410cec349f23"),
+            Country = "Poland",
+            City = "Gdańsk",
+            PostalCode = "80-342",
+            StreetName = "Grunwaldzka",
+            PropertyNumber = 11
+        });
     }
 }
