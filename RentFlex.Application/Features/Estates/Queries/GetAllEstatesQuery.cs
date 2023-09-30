fb233118547e489020a94606570c93caf8b8d061
Tomasz Krzemiński <7beat123@wp.pl>
2023-09-30 21:52:14 +0200

    Test

--- RentFlex.Application/Features/Estates/Queries/GetAllEstatesQuery.cs
+++ RentFlex.Application/Features/Estates/Queries/GetAllEstatesQuery.cs
@@ -1,12 +1,22 @@
 ﻿using MediatR;
+using RentFlex.Application.Contracts.Persistence;
 
 namespace RentFlex.Application.Features.Estates.Queries;
 public record GetAllEstatesQuery(Guid OwnerId) : IRequest<IEnumerable<EstateDto>>;
 
 internal class GetAllEstatesQueryHandler : IRequestHandler<GetAllEstatesQuery, IEnumerable<EstateDto>>
 {
+    private readonly IUnitOfWork unitOfWork;
+
+    public GetAllEstatesQueryHandler(IUnitOfWork unitOfWork)
+    {
+        this.unitOfWork = unitOfWork;
+    }
+
     public async Task<IEnumerable<EstateDto>> Handle(GetAllEstatesQuery request, CancellationToken cancellationToken)
     {
-        return new List<EstateDto>(); // ToDo: Implement
+        var estates = await unitOfWork.Estates.FindAllAsync(cancellationToken);
+
+        return new List<EstateDto>(); // Add mapping!
     }
 }
