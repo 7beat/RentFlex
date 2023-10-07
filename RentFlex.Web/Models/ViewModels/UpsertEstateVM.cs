7d4e3450fc919ad60285b75b15c175ad6d92426f
Tomasz Krzemiński <7beat123@wp.pl>
2023-10-07 23:40:53 +0200

    Added UpsertEstateVM

---
+++ RentFlex.Web/Models/ViewModels/UpsertEstateVM.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using RentFlex.Application.Features.Estates.Commands;
+
+namespace RentFlex.Web.Models.ViewModels;
+
+public class UpsertEstateVM
+{
+    public UpsertEstateCommand Estate { get; set; } = default!;
+    [ValidateNever]
+    public IEnumerable<SelectListItem> EstateType { get; set; } = default!;
+}
