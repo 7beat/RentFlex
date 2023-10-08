1e2927a8025b726785cc3aa464f09ccb9eedc542
Tomasz Krzemiński <7beat123@wp.pl>
2023-10-08 16:06:19 +0200

    Added concept Upsert

--- RentFlex.Web/Areas/Owner/Controllers/EstateController.cs
+++ RentFlex.Web/Areas/Owner/Controllers/EstateController.cs
@@ -1,25 +1,90 @@
-﻿using MediatR;
+﻿using AutoMapper;
+using MediatR;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using NuGet.Packaging;
+using RentFlex.Application.Features.Estates.Commands;
 using RentFlex.Application.Features.Estates.Queries;
+using RentFlex.Web.Models.ViewModels;
+using System.Collections.ObjectModel;
 using System.Security.Claims;
 
 namespace RentFlex.Web.Areas.Owner.Controllers;
+[Authorize]
 [Area("Owner")]
 public class EstateController : Controller
 {
     private readonly IMediator _mediator;
-    public EstateController(IMediator mediator)
+    private readonly IMapper _mapper;
+    private readonly IWebHostEnvironment _webHostEnvironment;
+    public EstateController(IMediator mediator, IMapper mapper, IWebHostEnvironment webHostEnvironment)
     {
         _mediator = mediator;
+        _mapper = mapper;
+        _webHostEnvironment = webHostEnvironment;
     }
 
-    [Authorize]
     public async Task<IActionResult> Index()
     {
         var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
         var estates = await _mediator.Send(new GetAllEstatesQuery(Guid.Parse(userId!)));
 
         return View(estates);
+    }
+
+    public async Task<IActionResult> Upsert(Guid? id)
+    {
+        UpsertEstateVM estateVM = new()
+        {
+            Estate = new(),
+        };
+
+        if (id is not null)
+        {
+            var estateDto = await _mediator.Send(new GetSingleEstateQuery(id.Value));
+            estateVM.Estate = _mapper.Map<UpsertEstateCommand>(estateDto);
+        }
+
+        return View(estateVM);
+    }
+
+    [HttpPost]
+    [DisableRequestSizeLimit]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> Upsert(UpsertEstateVM estateVM, List<IFormFile> estateImages)
+    {
+        if (ModelState.IsValid)
+        {
+            estateVM.Estate.OwnerId = new Guid(User.FindFirstValue(ClaimTypes.NameIdentifier)!);
+            estateVM.Estate.ImageUrls.AddRange(PersistImages(estateImages));
+
+            await _mediator.Send(estateVM.Estate);
+            return RedirectToAction(nameof(Index));
+        }
+
+        return RedirectToAction(nameof(Index));
+    }
+
+    private IEnumerable<string> PersistImages(List<IFormFile> images)
+    {
+        var imagePaths = new Collection<string>();
+        string wwwRootPath = _webHostEnvironment.WebRootPath;
+
+
+        foreach (var file in images)
+        {
+            string fileName = Guid.NewGuid().ToString();
+            var uploads = Path.Combine(wwwRootPath, @"images\estates");
+            var extension = Path.GetExtension(file.FileName);
+
+            using (var fileStreams = new FileStream(Path.Combine(uploads, fileName + extension), FileMode.Create))
+            {
+                file.CopyTo(fileStreams);
+            }
+
+            imagePaths.Add(@"\images\estates\" + fileName + extension);
+        }
+
+        return imagePaths;
     }
 }
