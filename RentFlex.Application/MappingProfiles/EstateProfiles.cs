28545e02fb53a1b7ca5432e0aa2ccf78e78d631f
Tomasz Krzemiński <7beat123@wp.pl>
2023-10-09 00:19:30 +0200

    Fixed mappings

--- RentFlex.Application/MappingProfiles/EstateProfiles.cs
+++ RentFlex.Application/MappingProfiles/EstateProfiles.cs
@@ -11,11 +11,21 @@ internal class EstateProfiles : Profile
         CreateMap<Estate, EstateDto>()
             .ForMember(dest => dest.Country, opt => opt.MapFrom(src => src.Address.Country))
             .ForMember(dest => dest.City, opt => opt.MapFrom(src => src.Address.City))
+            .ForMember(dest => dest.PostalCode, opt => opt.MapFrom(src => src.Address.PostalCode))
             .ForMember(dest => dest.StreetName, opt => opt.MapFrom(src => src.Address.StreetName))
             .ForMember(dest => dest.PropertyNumber, opt => opt.MapFrom(src => src.Address.PropertyNumber));
 
         CreateMap<UpsertEstateCommand, Estate>()
+            .ForMember(dest => dest.Address, opt => opt.MapFrom(src => new Address
+            {
+                Country = src.Country!,
+                City = src.City!,
+                PostalCode = src.PostalCode!,
+                StreetName = src.StreetName!,
+                PropertyNumber = src.PropertyNumber.HasValue ? (int)src.PropertyNumber : default
+            }))
             .ReverseMap();
+
 
         CreateMap<UpsertEstateCommand, EstateDto>()
             .ReverseMap();
