8ec76e2fc8e2442c1b5f7f2cc83c69c06284dcba
Tomasz Krzemiński <7beat123@wp.pl>
2023-10-22 14:44:53 +0200

    Test Response

---
+++ RentFlex.Utility/WireMock/Responses/TestResponse.cs
@@ -0,0 +1,7 @@
+﻿namespace RentFlex.Utility.WireMock.Responses;
+internal class TestResponse
+{
+    public int Id { get; set; } = 1;
+    public string FirstName { get; set; } = "John";
+    public string LastName { get; set; } = "Doe";
+}
