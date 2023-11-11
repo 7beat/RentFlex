90fb766ccc13896a6bfad7ede403b50865b28765
Tomasz Krzemiński <7beat123@wp.pl>
2023-11-11 12:33:51 +0100

    Configured Airbnb endpoints

--- RentFlex.Utility/WireMock/WireMockService.cs
+++ RentFlex.Utility/WireMock/WireMockService.cs
@@ -8,7 +8,7 @@ using WireMock.Settings;
 namespace RentFlex.Utility.WireMock;
 public static class WireMockService
 {
-    private static WireMockServer wireMockServer;
+    private static WireMockServer wireMockServer = null!;
     public static void Start()
     {
         Console.WriteLine("Starting WireMock server!");
@@ -20,6 +20,7 @@ public static class WireMockService
         wireMockServer = WireMockServer.Start(settings);
 
         wireMockServer.ConfigureEndpoints();
+        wireMockServer.ConfigureAirbnbEndpoints();
     }
 
     private static void ConfigureEndpoints(this WireMockServer wireMockServer)
@@ -32,5 +33,18 @@ public static class WireMockService
         wireMockServer.Given(Request.Create().WithPath("/api/rentals").UsingGet())
             .RespondWith(Response.Create().WithStatusCode(200).WithBody(jsonResponse));
 
+    }
+
+    private static void ConfigureAirbnbEndpoints(this WireMockServer wireMockServer)
+    {
+        // GetSingleAirbnbEstate, airbnbReference/estates/estateId
+        var jsonSingleAirbnbResponse = JsonConvert.SerializeObject(ResponseGenerator.GetSingleAirbnbEstateResponse(), Formatting.Indented);
+        wireMockServer.Given(Request.Create().WithPath($"/airbnb/88e3ffd5-0de9-487b-a053-da87bcca62cf/estates/7579550f-a641-457d-ba59-47e31c87dbee").UsingGet())
+            .RespondWith(Response.Create().WithStatusCode(200).WithBody(jsonSingleAirbnbResponse));
+
+        // GetAllAirbnbEstates
+        var jsonAllAirbnbEstates = JsonConvert.SerializeObject(ResponseGenerator.GetAllAirbnbEstatesResponse(), Formatting.Indented);
+        wireMockServer.Given(Request.Create().WithPath($"/airbnb/88e3ffd5-0de9-487b-a053-da87bcca62cf/estates").UsingGet())
+            .RespondWith(Response.Create().WithStatusCode(200).WithBody(jsonAllAirbnbEstates));
     }
 }
