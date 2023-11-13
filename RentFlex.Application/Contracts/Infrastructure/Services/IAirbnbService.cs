e9ed60a5bd26f501ef9829c832746462c3d0057e
Tomasz Krzemiński <7beat123@wp.pl>
2023-11-13 20:08:34 +0100

    Added requests

--- RentFlex.Application/Contracts/Infrastructure/Services/IAirbnbService.cs
+++ RentFlex.Application/Contracts/Infrastructure/Services/IAirbnbService.cs
@@ -4,6 +4,8 @@ namespace RentFlex.Application.Contracts.Infrastructure.Services;
 public interface IAirbnbService
 {
     Task Test();
-    Task<IEnumerable<Estate>> GetAllEstatesAsync(Guid airbnbReference);
+    Task<IEnumerable<Estate>> GetAllEstatesAsync(Guid airbnbReference); // userReference
     Task<Guid> CreateEstateAsync(Estate estate);
+    Task<bool> UpdateEstateAsync(Guid airbnbReference, Estate estate);
+    Task<bool> DeleteEstateAsync(Guid airbnbReference); // estateReference
 }
