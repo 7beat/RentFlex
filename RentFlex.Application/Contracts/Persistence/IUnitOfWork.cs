daa07338a25f0c615a48f9b381572c1796db0923
Tomasz Krzemiński <7beat123@wp.pl>
2023-11-18 14:58:37 +0100

    Added IUser repo

--- RentFlex.Application/Contracts/Persistence/IUnitOfWork.cs
+++ RentFlex.Application/Contracts/Persistence/IUnitOfWork.cs
@@ -4,5 +4,6 @@ namespace RentFlex.Application.Contracts.Persistence;
 public interface IUnitOfWork
 {
     IEstateRepository Estates { get; }
+    IUserRepository Users { get; }
     Task<bool> SaveChangesAsync(CancellationToken cancellationToken = default!);
 }
