79df2be8b0e5fd9696d453aa73c5a733e8a49b4e
Tomasz Krzemiński <7beat123@wp.pl>
2023-11-19 19:33:13 +0100

    Migration

---
+++ RentFlex.Infrastructure/Migrations/20231119183059_AddedRentalsConfiguration.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RentFlex.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddedRentalsConfiguration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "RentType",
+                table: "Rentals",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.InsertData(
+                table: "Rentals",
+                columns: new[] { "Id", "EndDate", "EstateId", "RentType", "StartDate" },
+                values: new object[] { new Guid("c36678ef-4941-42cc-913f-03e3b0608238"), new DateTime(2023, 11, 22, 0, 0, 0, 0, DateTimeKind.Unspecified), new Guid("555daf1f-c760-48d4-9fcf-410cec349f23"), "ShortTerm", new DateTime(2023, 11, 19, 0, 0, 0, 0, DateTimeKind.Unspecified) });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Rentals",
+                keyColumn: "Id",
+                keyValue: new Guid("c36678ef-4941-42cc-913f-03e3b0608238"));
+
+            migrationBuilder.AlterColumn<int>(
+                name: "RentType",
+                table: "Rentals",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+        }
+    }
+}
