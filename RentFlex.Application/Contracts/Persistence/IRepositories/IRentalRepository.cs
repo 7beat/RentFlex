7825ef37ba8189710af583c6c4141f9fe3481ef3
Tomasz Krzemiński <7beat123@wp.pl>
2023-11-20 21:09:55 +0100

    Added RentalRepository

---
+++ RentFlex.Application/Contracts/Persistence/IRepositories/IRentalRepository.cs
@@ -0,0 +1,7 @@
+﻿using RentFlex.Domain.entities;
+
+namespace RentFlex.Application.Contracts.Persistence.IRepositories;
+public interface IRentalRepository : IGenericRepository<Rental>
+{
+    void Update(Rental rental);
+}
