194ecb98ff67889e773251c04bfc819493f16b67
Tomasz Krzemiński <7beat123@wp.pl>
2023-11-20 20:16:49 +0100

    Tweak

--- RentFlex.Application/Contracts/Persistence/IRepositories/IEstateRepository.cs
+++ RentFlex.Application/Contracts/Persistence/IRepositories/IEstateRepository.cs
@@ -4,5 +4,4 @@ namespace RentFlex.Application.Contracts.Persistence.IRepositories;
 public interface IEstateRepository : IGenericRepository<Estate>
 {
     void Update(Estate estate);
-    //Task<Estate> FindWithOwnerDetails();
 }
