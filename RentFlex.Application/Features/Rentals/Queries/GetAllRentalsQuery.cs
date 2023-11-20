a30142631e6ff4ea65e83f8e14ed23d54eab5361
Tomasz Krzemiński <7beat123@wp.pl>
2023-11-20 21:10:29 +0100

    Added GetAllRentalsQuery

---
+++ RentFlex.Application/Features/Rentals/Queries/GetAllRentalsQuery.cs
@@ -0,0 +1,28 @@
+﻿using AutoMapper;
+using MediatR;
+using RentFlex.Application.Contracts.Persistence;
+using RentFlex.Application.Models;
+
+namespace RentFlex.Application.Features.Rentals.Queries;
+public record GetAllRentalsQuery(string UserId) : IRequest<IEnumerable<RentalDto>>
+{
+    internal class GetAllRentalsQueryHandler : IRequestHandler<GetAllRentalsQuery, IEnumerable<RentalDto>>
+    {
+        private readonly IUnitOfWork unitOfWork;
+        private readonly IMapper mapper;
+
+        public GetAllRentalsQueryHandler(IUnitOfWork unitOfWork, IMapper mapper)
+        {
+            this.unitOfWork = unitOfWork;
+            this.mapper = mapper;
+        }
+
+        public async Task<IEnumerable<RentalDto>> Handle(GetAllRentalsQuery request, CancellationToken cancellationToken)
+        {
+            var rentals = await unitOfWork.Rentals.FindAllAsync(r => r.Estate.UserId == request.UserId);
+            var rentalsDto = mapper.Map<IEnumerable<RentalDto>>(rentals);
+            return rentalsDto;
+        }
+    }
+}
+
