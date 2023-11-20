d70fe4a7117447a41095b021c187725160b31613
Tomasz Krzemiński <7beat123@wp.pl>
2023-11-20 21:34:05 +0100

    Added Controller

---
+++ RentFlex.Web/Areas/Owner/Controllers/RentalController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RentFlex.Web.Areas.Owner.Controllers;
+public class RentalController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
