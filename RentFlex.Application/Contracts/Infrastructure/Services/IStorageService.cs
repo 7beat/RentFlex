b8ac5aac0cff077fbfb4de40e800161e613c755b
Tomasz Krzemiński <7beat123@wp.pl>
2024-05-30 22:19:33 +0200

    Added StorageService

---
+++ RentFlex.Application/Contracts/Infrastructure/Services/IStorageService.cs
@@ -0,0 +1,6 @@
+﻿namespace RentFlex.Application.Contracts.Infrastructure.Services;
+public interface IStorageService
+{
+    Task<string> AddAsync(Stream stream, CancellationToken cancellationToken = default);
+    Task<bool> DeleteAsync(string blobUrl, CancellationToken cancellationToken = default);
+}
---
+++ RentFlex.Infrastructure/Services/StorageService.cs
@@ -0,0 +1,32 @@
+﻿using Azure.Storage.Blobs;
+using RentFlex.Application.Contracts.Infrastructure.Services;
+
+namespace RentFlex.Infrastructure.Services;
+public class StorageService(BlobServiceClient blobServiceClient) : IStorageService
+{
+    public async Task<string> AddAsync(Stream stream, CancellationToken cancellationToken)
+    {
+        var containerClient = blobServiceClient.GetBlobContainerClient("images");
+        await containerClient.CreateIfNotExistsAsync();
+
+        var blobClient = containerClient.GetBlobClient(Guid.NewGuid().ToString());
+
+        var response = await blobClient.UploadAsync(stream, cancellationToken);
+
+        return blobClient.Uri.AbsoluteUri;
+    }
+
+    public async Task<bool> DeleteAsync(string blobUrl, CancellationToken cancellationToken)
+    {
+        var uri = new Uri(blobUrl);
+        var blobName = uri.Segments.Last();
+
+        var containerClient = blobServiceClient.GetBlobContainerClient("images");
+        var blobClient = containerClient.GetBlobClient(blobName);
+
+        var response = await blobClient.DeleteIfExistsAsync(cancellationToken: cancellationToken);
+
+        return response.Value;
+    }
+
+}
