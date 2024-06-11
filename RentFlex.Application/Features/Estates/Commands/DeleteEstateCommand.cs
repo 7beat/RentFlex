179788e97ede209ad4c7375ae562a8c8ad70a522
Tomasz Krzemiński <7beat123@wp.pl>
2024-06-11 19:27:24 +0200

    Added image delete

--- RentFlex.Application/Features/Estates/Commands/DeleteEstateCommand.cs
+++ RentFlex.Application/Features/Estates/Commands/DeleteEstateCommand.cs
@@ -1,4 +1,5 @@
 ﻿using MediatR;
+using RentFlex.Application.Contracts.Infrastructure.Services;
 using RentFlex.Application.Contracts.Persistence;
 
 namespace RentFlex.Application.Features.Estates.Commands;
@@ -7,16 +8,23 @@ public record DeleteEstateCommand(Guid Id) : IRequest<bool>;
 internal class DeleteEstateCommandHandler : IRequestHandler<DeleteEstateCommand, bool>
 {
     private readonly IUnitOfWork unitOfWork;
+    private readonly IStorageService storageService;
 
-    public DeleteEstateCommandHandler(IUnitOfWork unitOfWork)
+    public DeleteEstateCommandHandler(IUnitOfWork unitOfWork, IStorageService storageService)
     {
         this.unitOfWork = unitOfWork;
+        this.storageService = storageService;
     }
 
     public async Task<bool> Handle(DeleteEstateCommand request, CancellationToken cancellationToken)
     {
         var estate = await unitOfWork.Estates.FindSingleAsync(e => e.Id == request.Id, cancellationToken);
+        foreach (var imageUrl in estate!.ImageUrls)
+        {
+            await storageService.DeleteAsync(imageUrl, cancellationToken);
+        }
         unitOfWork.Estates.Remove(estate!);
+
         return await unitOfWork.SaveChangesAsync(cancellationToken);
     }
 }
