c37c7fa94271ef29d9c42bf7dc2d300dbdf1a75b
Tomasz Krzemiński <7beat123@wp.pl>
2025-01-04 21:34:22 +0100

    Added consts

---
+++ RentFlex.Application/Constants/WebConstants.cs
@@ -0,0 +1,7 @@
+﻿namespace RentFlex.Application.Constants;
+public static class WebConstants
+{
+    public const string OwnerArea = "Owner";
+    public const string HomeController = "Home";
+    public const string IndexAction = "Index";
+}
