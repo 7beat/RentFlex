a329a74402239375295815394ba42a367b31baf3
Tomasz Krzemiński <7beat123@wp.pl>
2025-10-16 20:49:15 +0200

    Added PDF View generation and download

---
+++ RentFlex.Application/Features/Rentals/Queries/GetSingleRentalQuery.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using MediatR;
+using RentFlex.Application.Contracts.Persistence;
+using RentFlex.Application.Models;
+
+namespace RentFlex.Application.Features.Rentals.Queries;
+public record GetSingleRentalQuery(Guid InvoiceId) : IRequest<RentalDto>;
+
+internal class GetSingleRentalQueryHandler(IUnitOfWork unitOfWork, IMapper mapper) : IRequestHandler<GetSingleRentalQuery, RentalDto>
+{
+    public async Task<RentalDto> Handle(GetSingleRentalQuery request, CancellationToken cancellationToken)
+    {
+        var rental = await unitOfWork.Rentals.FindSingleAsync(r => r.Id == request.InvoiceId, cancellationToken);
+
+        return mapper.Map<RentalDto>(rental);
+    }
+}
--- RentFlex.Application/Models/RentalDto.cs
+++ RentFlex.Application/Models/RentalDto.cs
@@ -3,9 +3,10 @@
 namespace RentFlex.Application.Models;
 public class RentalDto
 {
+    public Guid Id { get; set; }
     public DateOnly StartDate { get; set; }
     public DateOnly EndDate { get; set; }
-    public RentType RentType { get; set; } // Rental Should have CostPerDay
+    public RentType RentType { get; set; }
     public string PropertyName { get; set; } = default!;
-    //public string EstateDescription { get; set; }
+    public double CostPerDay { get; set; }
 }
